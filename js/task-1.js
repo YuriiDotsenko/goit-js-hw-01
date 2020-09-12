efb034977fe7b522d18e7caf7225da24e1e5fe8f
YuriiDotsenko <yuriidotsenko59@gmail.com>
2020-09-12 19:11:25 +0200

    Зробив завдання №1

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const selected = "Выбран:";
+const name = "Генератор защитного поля";
+let price = "цена: 1000 кредитов";
+
+console.log(selected, name, price);
+
+const newPrice = "цена: 2000 кредитов";
+
+console.log(selected, name, newPrice);
---
+++ js/task-2.js
@@ -0,0 +1,6 @@
+const total = 100;
+const ordered = 50;
+
+for (let i = total) {
+  console.log();
+}
